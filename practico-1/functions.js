1b283e4b90b1950d6139b8c079dddbe772739600
Gonzalo Rosano <grosano@quanam.com>
2025-04-07 15:51:32 -0300

    actualizacion de imports varios

--- practico-1/functions.js
+++
@@ -1,7 +0,0 @@
-export function obtenerElemento(id) {
-  return document.querySelector(`#${id}`);
-}
-
-export function addEventListenerToButton(id, callback) {
-  document.querySelector(`#${id}`).addEventListener("click", callback);
-}
