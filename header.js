013e5f4789276c0e29a15d6cf29caebd8de042fa
Gonzalo Rosano <rosanogonzalo@gmail.com>
2025-04-08 00:05:41 -0300

    componetizacion y actualizacion de header

---
+++ header.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const headerHTML = `
+      <header>
+        <a href="/">
+          <figure>
+            <img src="/img/logo.webp" alt="Logo">
+            <figcaption>Gonzalo Rosano</figcaption>
+          </figure>
+        </a>
+        <nav>
+          <ul>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/programacion">Programación</a></li>
+            <li><a href="/diw">Diseño de interfaz web</a></li>
+          </ul>
+        </nav>
+      </header>
+    `;
+  
+    const container = document.createElement("div");
+    container.innerHTML = headerHTML;
+  
+    const main = document.body;
+    const firstElement = main.querySelector("h1, h2, main, section");
+    main.insertBefore(container, firstElement || main.firstChild);
+  });
+  
