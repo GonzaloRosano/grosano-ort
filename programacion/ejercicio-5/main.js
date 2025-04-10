5dca77496d9047d2f7aa7fae951833e01bf6a0e6
Gonzalo Rosano <grosano@quanam.com>
2025-04-10 08:35:38 -0300

    mejoras en formulario y limpieza de campo para ingresar el numero

--- programacion/ejercicio-5/main.js
+++ programacion/ejercicio-5/main.js
@@ -1,17 +1,18 @@
-document.querySelector("#btnAdivinar").addEventListener("click", adivinar);
-
+const btnAdivinar = document.querySelector("#btnAdivinar");
+btnAdivinar.addEventListener("click", adivinar);
 let numSecreto = (Math.random() * 100 + 1) | 0;
 let intentos = 0;
 
 console.log(numSecreto);
 function adivinar() {
-  let numero = Number(document.querySelector("#txtNumero").value);
+  const txtNumero = document.querySelector("#txtNumero");
+  let numero = Number(txtNumero.value);
   let msgIntentos = document.querySelector("#msgIntentos");
   intentos++;
   if (numero == numSecreto) {
     msgIntentos.innerHTML = intentos;
     document.querySelector("#msgResultado").innerHTML = "Has adivinado";
-    document.querySelector("#btnAdivinar").disabled = true;
+    btnAdivinar.disabled = true;
   } else if (numero > numSecreto) {
     msgIntentos.innerHTML = intentos;
     document.querySelector("#msgPista").innerHTML = "El numero es menor";
@@ -19,4 +20,5 @@ function adivinar() {
     msgIntentos.innerHTML = intentos;
     document.querySelector("#msgPista").innerHTML = "El numero es mayor";
   }
+  txtNumero.value = "";
 }
